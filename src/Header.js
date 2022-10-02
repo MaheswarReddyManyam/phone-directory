00cc3759ee401141a1ebe54fe20f41ef84ec2b54
MaheswarReddyManyam <manyammahesh2499@gmail.com>
2022-10-02 21:55:14 +0530

    Styled 'Header' (External Styling)

--- src/Header.js
+++ src/Header.js
@@ -1,22 +1,11 @@
 import React from "react";
+import './index.css';
 
-// class Header extends Component{      ------> Class component
-//     render(){
-//         return (
-//             <div className="header">
-//                 Phone Directory
-//             </div>
-//         )
-//     }
-// }
-
-// ------> Functional component
 const Header= ()=>{
-    const headerStyle ={textAlign:'center', padding:20, background: 'black', color: 'white', textTransform: 'uppercase'}; 
     return (
-        <div className="header" style={headerStyle}>
-                Phone Directory
-            </div>
+        <div className="header">
+           Phone Directory
+        </div>
     )
 }
 export default Header;
